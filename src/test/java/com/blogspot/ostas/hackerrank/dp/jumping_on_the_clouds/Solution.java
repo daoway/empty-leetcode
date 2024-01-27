2c008d2fe7b54a21352cd3bdcaa5279fe12964a5
daoway <stas>
2024-01-28 01:06:05 +0200

    solution added

---
+++ src/test/java/com/blogspot/ostas/hackerrank/dp/jumping_on_the_clouds/Solution.java
@@ -0,0 +1,42 @@
+package com.blogspot.ostas.hackerrank.dp.jumping_on_the_clouds;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Write your code here
+        return 0;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
