979d599d941c6ac7a133cdbf2c42ec7ff18ea5bc
daoway <stas>
2024-01-28 01:15:48 +0200

    solution added

---
+++ src/test/java/com/blogspot/ostas/hackerrank/dp/jumping_on_the_clouds/SolutionTest.java
@@ -0,0 +1,17 @@
+package com.blogspot.ostas.hackerrank.dp.jumping_on_the_clouds;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+    @Test
+    void example_0() {
+        List<Integer> c = List.of(0, 0, 1, 0, 0, 1, 0);
+        int result = Result.jumpingOnClouds(c);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+}
