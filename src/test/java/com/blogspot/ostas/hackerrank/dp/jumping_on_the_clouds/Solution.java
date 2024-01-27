95b87f2a09c73a5b6b950a4d6530577967cce804
daoway <stas>
2024-01-28 01:58:50 +0200

    solution added

--- src/test/java/com/blogspot/ostas/hackerrank/dp/jumping_on_the_clouds/Solution.java
+++ src/test/java/com/blogspot/ostas/hackerrank/dp/jumping_on_the_clouds/Solution.java
@@ -16,8 +16,15 @@ class Result {
      */
 
     public static int jumpingOnClouds(List<Integer> c) {
-        // Write your code here
-        return 0;
+        int n = c.size();
+        int max = Integer.MAX_VALUE - 200;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = (c.get(1) == 1) ? max : 1;
+        for (int i = 2; i < n; i++) {
+            dp[i] = (c.get(i) == 1) ? max : 1 + Math.min(dp[i - 1], dp[i - 2]);
+        }
+        return dp[n - 1];
     }
 
 }
@@ -26,6 +33,8 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
                 .map(Integer::parseInt)
--- src/test/java/com/blogspot/ostas/hackerrank/dp/jumping_on_the_clouds/SolutionTest.java
+++ src/test/java/com/blogspot/ostas/hackerrank/dp/jumping_on_the_clouds/SolutionTest.java
@@ -14,4 +14,12 @@ public class SolutionTest {
         int expected = 4;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void example_1() {
+        List<Integer> c = List.of(0, 0, 0, 1, 0, 0);
+        int result = Result.jumpingOnClouds(c);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
 }
