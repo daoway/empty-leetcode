6abb3fb1ac409301823530a44e689126e5b47e49
daoway <stas>
2024-01-28 20:39:36 +0200

    solution added

--- src/test/java/com/blogspot/ostas/leetcode/all/medium/longest_increasing_subsequence/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/longest_increasing_subsequence/Solution.java
@@ -1,5 +1,7 @@
 package com.blogspot.ostas.leetcode.all.medium.longest_increasing_subsequence;
 
+import java.util.Arrays;
+
 /*
       300. longest increasing subsequence
 
@@ -11,7 +13,29 @@ subsequence
 
 */
 class Solution {
-  public int lengthOfLIS(int[] nums) {
-    return 0;
-  }
+    private int max(int[] L) {
+        int maxLength = Integer.MIN_VALUE;
+        for (final int length : L) {
+            maxLength = Math.max(maxLength, length);
+        }
+        return maxLength;
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] L = new int[n];
+        // Initialize the array with minimum length 1 for each index
+        Arrays.fill(L, 1);
+
+        // Iterate to fill in the values of L(i) using the recurrence relation
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && L[i] < L[j] + 1) {
+                    L[i] = L[j] + 1;
+                }
+            }
+        }
+        // Find the maximum value in the array L
+        return max(L);
+    }
 }
