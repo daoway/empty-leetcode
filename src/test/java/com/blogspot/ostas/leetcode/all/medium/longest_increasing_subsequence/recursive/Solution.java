446dfab85acfa81c4f9737c453dd2df23861dbc7
daoway <stas>
2024-02-01 19:40:09 +0200

    solution

--- src/test/java/com/blogspot/ostas/leetcode/all/medium/longest_increasing_subsequence/recursive/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/longest_increasing_subsequence/recursive/Solution.java
@@ -1,26 +1,30 @@
 package com.blogspot.ostas.leetcode.all.medium.longest_increasing_subsequence.recursive;
 
 public class Solution {
-    private int maxUltimate;
-
+    private int max;
     public int lengthOfLISHelper(int[] arr, int n) {
+        // Base case: if there is only one element, the LIS length is 1
         if (n == 1) {
             return 1;
         }
         int currResult;
-        int maxEndingHere = 1;
+        int maxEnding = 1;
+
         for (int i = n - 1; i > 0; i--) {
+            // Recursively calculate LIS for previous elements
             currResult = lengthOfLISHelper(arr, i);
-            if ((arr[i - 1] < arr[n - 1]) && (currResult + 1 > maxEndingHere)) {
-                maxEndingHere = currResult + 1;
+
+            // Check if the current element can be included in the increasing subsequence
+            if ((arr[i - 1] < arr[n - 1]) && (currResult + 1 > maxEnding)) {
+                maxEnding = currResult + 1;
             }
         }
-        maxUltimate = Math.max(maxEndingHere, maxUltimate);
-        return maxEndingHere;
+        max = Math.max(maxEnding, max);
+        return maxEnding;
     }
 
     public int lengthOfLIS(int[] nums) {
         lengthOfLISHelper(nums, nums.length);
-        return maxUltimate;
+        return max;
     }
 }
