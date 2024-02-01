887cd3bb768ea1333e3b425f20e7c66270c0ae59
daoway <stas>
2024-02-01 23:21:53 +0200

    solution

--- src/test/java/com/blogspot/ostas/hackerrank/dp/knapsack/SolutionTest.java
+++ src/test/java/com/blogspot/ostas/hackerrank/dp/knapsack/SolutionTest.java
@@ -4,13 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     @Test
-    void test_0(){
-        int k=0;
-        List<Integer> arr = List.of();
+    void test_0() {
+        int k = 12;
+        List<Integer> arr = List.of(1, 6, 9);
         var result = Result.unboundedKnapsack(k, arr);
+        var expected = 12;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test_1() {
+        int k = 9;
+        List<Integer> arr = List.of(3, 4, 4, 4, 8);
+        var result = Result.unboundedKnapsack(k, arr);
+        var expected = 9;
+        assertThat(result).isEqualTo(expected);
     }
 }
