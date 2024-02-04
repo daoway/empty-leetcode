404294ed6169668997436ad1781dc72aa7c6078f
daoway <stas>
2024-02-04 20:05:31 +0200

    solution

--- src/test/java/com/blogspot/ostas/leetcode/all/medium/coin_change/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/coin_change/Solution.java
@@ -30,7 +30,7 @@ class Solution {
      */
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (final int coin : coins) {
