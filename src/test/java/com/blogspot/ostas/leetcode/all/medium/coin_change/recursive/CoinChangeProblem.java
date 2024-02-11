7991015d5676dfda5a5d940494c1bbabc429569c
daoway <stas>
2024-02-12 01:15:18 +0200

    temp

--- src/test/java/com/blogspot/ostas/leetcode/all/medium/coin_change/recursive/CoinChangeProblem.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/coin_change/recursive/CoinChangeProblem.java
@@ -1,5 +1,7 @@
 package com.blogspot.ostas.leetcode.all.medium.coin_change.recursive;
 
+import java.util.Arrays;
+
 public class CoinChangeProblem {
 
     public static void main(String[] args) {
@@ -9,6 +11,11 @@ public class CoinChangeProblem {
         var x1 = countWays(100, new int[]{1, 2, 5, 10, 25, 50});
         System.out.println("total count : " + x1);
 
+        var x2 = change(100, new int[]{1, 2, 5, 10, 25, 50});
+        System.out.println("total count : " + x2);
+
+        var x3 = countWays(100, new int[]{1, 2, 5, 10, 25, 50});
+        System.out.println("total count : " + x3);
     }
 
     // T(n) = O(amount^coinCount)
@@ -33,7 +40,7 @@ public class CoinChangeProblem {
 
         return count;
     }
-
+///////////////////////////////////////////////////////////////////////////////////////
     //added params coins[] but for loops is same as before
     static int countWaysX(int amount, int[] coins) {
         int count = 0;
@@ -56,7 +63,7 @@ public class CoinChangeProblem {
 
         return count;
     }
-
+/////////////////////////////////////////////////////////////////////
     static int countWays(int amount, int[] coins) {
         int[] coinCount = new int[coins.length];
         return countWaysHelper(amount, coins, coinCount, 0);
@@ -76,9 +83,65 @@ public class CoinChangeProblem {
         for (int i = 0; i <= maxCount; i++) {
             coinCount[index] = i;
             ways += countWaysHelper(remainingAmount - i * coins[index], coins, coinCount, index + 1);
-            System.out.println(ways);
         }
         return ways;
     }
+///////////////////////////////////////////////////////////////////////////////////
 
+    //https://faangmaster.medium.com/%D1%87%D0%B8%D1%81%D0%BB%D0%BE-%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1%D0%BE%D0%B2-%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D0%BD%D1%8F%D1%82%D1%8C-%D0%B4%D0%B5%D0%BD%D1%8C%D0%B3%D0%B8-8c295519be35
+    public static int change(int amount, int[] coins) {
+        if (amount == 0) {
+            return 1;
+        }
+
+        if (coins.length == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[amount + 1][coins.length];
+
+        //Заполняем первую строку
+        Arrays.fill(dp[0], 1);
+
+        //Заполняем первый столбец
+        for (int i = 1; i <= amount; i++) {
+            dp[i][0] = (i < coins[0]) ? 0 : dp[i - coins[0]][0];
+        }
+
+
+        //Вычисляем все остальные значения в матрице
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 1; j < coins.length; j++) {
+                dp[i][j] = dp[i][j - 1] + ((i < coins[j]) ? 0 : dp[i - coins[j]][j]);
+            }
+        }
+
+        return dp[amount][coins.length - 1];
+    }
+
+    static int coinChange(int amount, int[] coins) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+
+        // Base case: If amount is 0, there's one way to make change (using no coins)
+        for (int i = 0; i <= coins.length; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= coins.length; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j < coins[i - 1]) {
+                    // If the current amount is less than the coin value,
+                    // then we cannot include this coin, so copy the value from the row above.
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // Otherwise, the number of ways to make change includes:
+                    // 1. Not using the current coin (value from the row above).
+                    // 2. Using the current coin (value from the same row but with the amount reduced by the coin value).
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+
+        return dp[coins.length][amount];
+    }
 }
