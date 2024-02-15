77216aba00287cd02a672fc4a32cc351ad9fe409
daoway <stas>
2024-02-15 21:49:53 +0200

    solution

--- src/test/java/com/blogspot/ostas/leetcode/all/medium/triangle/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/triangle/Solution.java
@@ -1,8 +1,5 @@
 package com.blogspot.ostas.leetcode.all.medium.triangle;
 
-import java.util.Comparator;
-import java.util.List;
-
 /*
       120. triangle
 
@@ -12,8 +9,26 @@ Given a triangle array, return the minimum path sum from top to bottom.
 For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
 
 */
+
+import java.util.List;
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        return triangle.stream().map(row -> row.stream().min(Comparator.naturalOrder()).get()).mapToInt(Integer::intValue).sum();
+        int n = triangle.size();
+        int[] dp = new int[n];
+
+        // Initialize the dp array with the values of the last row
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        // Bottom-up approach to calculate minimum path sum
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+
+        return dp[0];
     }
 }
