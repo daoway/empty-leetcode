f39481e7c032160a28b6ec09e05aafca14747729
daoway <stas>
2024-05-07 15:49:49 +0300

    solution

--- src/test/java/com/blogspot/ostas/leetcode/all/easy/contains_duplicate_ii/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/easy/contains_duplicate_ii/Solution.java
@@ -1,5 +1,10 @@
 package com.blogspot.ostas.leetcode.all.easy.contains_duplicate_ii;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
       219. contains duplicate ii
 
@@ -10,6 +15,23 @@ Given an integer array nums and an integer k, return true if there are two disti
 */
 class Solution {
   public boolean containsNearbyDuplicate(int[] nums, int k) {
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      //map.computeIfAbsent(nums[i], map.getOrDefault(nums[i],0)+1 l -> new ArrayList<>()).add(i);
+      map.computeIfAbsent(nums[i], l -> new ArrayList<>()).add(i);
+    }
+    for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+      if (entry.getValue().size() >=2){
+        Integer[] list = entry.getValue().toArray(Integer[]::new);
+        for(int i=0;i<list.length;i++){
+          for (int j = 0; j < list.length && j!=i; j++) {
+            if(Math.abs(list[i]-list[j])<=k){
+              return true;
+            }
+          }
+        }
+      }
+    }
     return false;
   }
 }
