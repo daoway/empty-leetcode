44b66320e783c6bffb5c9c123091bc1dd09433c2
daoway <stas>
2024-05-10 11:08:38 +0300

    solution

---
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/range_sum_query_mutable/optimized/NumArray.java
@@ -0,0 +1,23 @@
+package com.blogspot.ostas.leetcode.all.medium.range_sum_query_mutable.optimized;
+
+class NumArray {
+    private final int[] sums;
+
+    public NumArray(int[] nums) {
+        this.sums = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+    }
+
+    public void update(int index, int val) {
+        int diff = val - (sums[index + 1] - sums[index]);
+        for (int i = index + 1; i < sums.length; i++) {
+            sums[i] += diff;
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sums[right + 1] - sums[left];
+    }
+}
---
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/range_sum_query_mutable/optimized/NumArrayTest.java
@@ -0,0 +1,19 @@
+package com.blogspot.ostas.leetcode.all.medium.range_sum_query_mutable.optimized;
+
+import com.blogspot.ostas.leetcode.all.medium.range_sum_query_mutable.fenwick.NumArray;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumArrayTest {
+
+    @Test
+    void sumRange() {
+        NumArray numArray = new NumArray(new int[]{1, 3, 5});
+        var x0 = numArray.sumRange(0, 2); // return 1 + 3 + 5 = 9
+        assertThat(x0).isEqualTo(9);
+        numArray.update(1, 2);   // nums = [1, 2, 5]
+        var x1 = numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
+        assertThat(x1).isEqualTo(8);
+    }
+}
