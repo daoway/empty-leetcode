d4d0424935881cafb307acea35853443cb7afb1e
daoway <stas>
2024-05-11 23:20:41 +0300

    solution

--- src/test/java/com/blogspot/ostas/leetcode/all/easy/reverse_vowels_of_a_string/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/easy/reverse_vowels_of_a_string/Solution.java
@@ -1,5 +1,10 @@
 package com.blogspot.ostas.leetcode.all.easy.reverse_vowels_of_a_string;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
       345. reverse vowels of a string
 
@@ -10,7 +15,42 @@ The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower an
 
 */
 class Solution {
+  class Letter{
+    public char letter;
+    public int index;
+    public boolean isVowel;
+    public Letter(char letter, int index, boolean isVowel) {
+      this.letter = letter;
+      this.index = index;
+      this.isVowel = isVowel;
+    }
+  }
   public String reverseVowels(String s) {
-    return null;
+    StringBuilder result = new StringBuilder(s);
+    List<Letter> letters = new ArrayList<>();
+    for (int i = 0; i < s.length(); i++) {
+      letters.add(new Letter(s.charAt(i),i, isVowel(s.charAt(i))));
+    }
+    List<Integer> vowelIndexes = new ArrayList<>();
+    for (Letter letter : letters) {
+      if(letter.isVowel) {
+        vowelIndexes.add(letter.index);
+      }
+    }
+    List<Integer> reverseVowelIndexes = vowelIndexes.reversed();
+    Map<Integer, Integer> indexMap = new HashMap<>();
+    for (int i=0;i<vowelIndexes.size();i++) {
+      indexMap.put(vowelIndexes.get(i), reverseVowelIndexes.get(i));
+    }
+
+    for (Map.Entry<Integer, Integer> entry : indexMap.entrySet()) {
+      result.setCharAt(entry.getKey(), s.charAt(entry.getValue()));
+    }
+
+    return result.toString();
+  }
+
+  private boolean isVowel(char c) {
+    return c == 'a' || c == 'e' ||  c == 'i' || c == 'o' || c == 'u';
   }
 }
