e5fa3e218f042411a7baf70f8dc0fd1d54b13be7
daoway <stas>
2024-05-15 18:54:12 +0300

    temp

--- src/test/java/com/blogspot/ostas/leetcode/all/easy/finding_three_digit_even_numbers/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/easy/finding_three_digit_even_numbers/Solution.java
@@ -1,5 +1,7 @@
 package com.blogspot.ostas.leetcode.all.easy.finding_three_digit_even_numbers;
 
+import java.util.*;
+
 /*
       2094. finding 3-digit even numbers
 
@@ -15,7 +17,30 @@ Return a sorted array of the unique integers.
 
 */
 class Solution {
-  public int[] findEvenNumbers(int[] digits) {
-    return null;
-  }
+    public int[] findEvenNumbers(int[] digits) {
+        Set<Integer> set = new HashSet<>();
+        for (int digit : digits) {
+            set.add(digit);
+        }
+        int min3Digit = 100;
+        int max3Digit = 999;
+        TreeSet<Integer> result = new TreeSet<>();
+        for (int i = min3Digit; i <= max3Digit; i++) {
+            var candidateDigits = numberToDigits(i);
+            if(set.containsAll(candidateDigits) && i % 2 == 0){
+                result.add(i);
+            }
+
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private Set<Integer> numberToDigits(int n) {
+        var result = new HashSet<Integer>();
+        while (n > 0) {
+            result.add(n % 10);
+            n /= 10;
+        }
+        return result;
+    }
 }
