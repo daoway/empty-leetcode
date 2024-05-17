57e7a27994a21af1f7ac06772685c92dad77706e
daoway <stas>
2024-05-17 19:33:01 +0300

    temp

---
+++ src/test/java/com/blogspot/ostas/leetcode/all/medium/find_all_duplicates_in_an_array/constant_memory/Solution.java
@@ -0,0 +1,26 @@
+package com.blogspot.ostas.leetcode.all.medium.find_all_duplicates_in_an_array.constant_memory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                result.add(index + 1);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+
+        // Optionally restore the original values (if needed)
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.abs(nums[i]);
+        }
+
+        return result;
+    }
+}
