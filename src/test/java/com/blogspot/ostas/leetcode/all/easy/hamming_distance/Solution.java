94b7a902f39229d35de8e28276b1f9e2142b95e6
daoway <stas>
2024-06-07 15:24:56 +0300

    temp

--- src/test/java/com/blogspot/ostas/leetcode/all/easy/hamming_distance/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/easy/hamming_distance/Solution.java
@@ -10,7 +10,22 @@ Given two integers x and y, return the Hamming distance between them.
 
 */
 class Solution {
-  public int hammingDistance(int x, int y) {
-    return 0;
-  }
+    public int hammingDistance(int x, int y) {
+        var binStringX = Integer.toBinaryString(x);
+        var binStringY = Integer.toBinaryString(y);
+
+        // Pad the shorter binary string with leading zeros
+        int maxLength = Math.max(binStringX.length(), binStringY.length());
+        binStringX = String.format("%" + maxLength + "s", binStringX).replace(' ', '0');
+        binStringY = String.format("%" + maxLength + "s", binStringY).replace(' ', '0');
+
+        int mid = Math.min(binStringX.length(), binStringY.length());
+        int distance = 0;
+        for (int i = 0; i < mid; i++) {
+            if (binStringX.charAt(i) != binStringY.charAt(i)) {
+                distance++;
+            }
+        }
+        return distance;
+    }
 }
