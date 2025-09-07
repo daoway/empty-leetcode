2ec4146c7ae779c273a16283fb413cb0b18fc675
daoway <stas>
2025-09-08 01:44:27 +0300

    temp

--- src/test/java/com/blogspot/ostas/leetcode/all/easy/transpose_matrix/Solution.java
+++ src/test/java/com/blogspot/ostas/leetcode/all/easy/transpose_matrix/Solution.java
@@ -11,6 +11,25 @@ The transpose of a matrix is the matrix flipped over its main diagonal, switchin
 */
 class Solution {
   public int[][] transpose(int[][] matrix) {
-    return null;
+    int n = matrix.length;
+    int m = matrix[0].length;
+    int[][] result = new int[m][n];
+    if(n == m){
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+          int temp = matrix[i][j];
+          matrix[i][j] = matrix[j][i];
+          matrix[j][i] = temp;
+        }
+      }
+      return matrix;
+    }else{
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          result[j][i] = matrix[i][j];
+        }
+      }
+      return result;
+    }
   }
 }
